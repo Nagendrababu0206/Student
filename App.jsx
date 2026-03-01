b9faebdcf672f55c3c9307a85f584fce464648b3
Nagendra.m <nagendra1841@gmail.com>
2026-03-01 22:18:59 +0530

    Fix deno lint by removing unused App.jsx imports

--- App.jsx
+++ App.jsx
@@ -1,7 +1,3 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Frontend from "./Frontend";
-import Signup from "./Signup";
-import Dashboard from "./Dashboard";
-import "./App.css";
-import { useState } from "react";
+export default function App() {
+  return null;
+}
